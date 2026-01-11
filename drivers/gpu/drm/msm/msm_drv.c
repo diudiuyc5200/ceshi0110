8550a23f1db20735bb2ff0b191d21afd0053fbdd
diudiuyc5200 <17766728994@163.com>
2026-01-11 08:38:03 +0800

    Add files via upload

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -907,8 +907,14 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 		priv->fbdev = msm_fbdev_init(ddev);
 #endif
 
-	if (!msm_debugfs_late_init(ddev)) {
-		sde_dbg_debugfs_register(dev);
+	ret = msm_debugfs_late_init(ddev);
+	if (ret)
+		goto fail;
+
+	ret = sde_dbg_debugfs_register(dev);
+	if (ret) {
+		dev_err(dev, "failed to reg sde dbg debugfs: %d\n", ret);
+		goto fail;
 	}
 
 	/* perform subdriver post initialization */
